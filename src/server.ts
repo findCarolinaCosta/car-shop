c62c208b20e7986b6404e97b424b4a3c7c9753e7
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-05-29 20:56:20 -0300

    implements motorcycle route

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,22 @@
 import App from './app';
-import { Car as ICar } from './interfaces/CarInterface';
-import CustomRoute from './routes';
 import CarController from './controllers/CarController';
+import MotorcycleController from './controllers/MotorcycleController';
+import { Car as ICar } from './interfaces/CarInterface';
+import { Motorcycle as IMotorcycle } from './interfaces/MotorcycleInterface';
+import CustomRoute from './routes';
 
 const server = new App();
 
 const carController = new CarController();
-const carRoute = new CustomRoute<ICar>();
-
-carRoute.addRoute(carController);
-
 const carRoutes = new CustomRoute<ICar>();
+
+const motorcycleController = new MotorcycleController();
+const MotorcycleRoutes = new CustomRoute<IMotorcycle>();
+
 carRoutes.addRoute(carController);
+MotorcycleRoutes.addRoute(motorcycleController);
 
 server.addRouter(carRoutes.router);
+server.addRouter(MotorcycleRoutes.router);
 
 export default server;
