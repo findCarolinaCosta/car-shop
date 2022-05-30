34b67b89df3f45d1e2e58a91832abda5302e339e
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-05-29 22:17:38 -0300

    refactor: MongooseMock used for test

--- src/tests/unit/models/utils/MongooseMock.ts
+++ src/tests/unit/models/utils/MongooseMock.ts
@@ -8,7 +8,7 @@ interface ICar {
   doorsQty?: number;
 }
 
-const data: ICar[] = [
+const dataCar: ICar[] = [
   {
     _id: "62905e472ab42302a46b5fdf",
     model: "Uno",
@@ -20,45 +20,85 @@ const data: ICar[] = [
   }
 ]
 
+interface IMotorcycle {
+  _id: string;
+  model: string;
+  year: number;
+  color: string;
+  buyValue: number;
+  category: "Custom" | "Street" | "Trail",
+  engineCapacity: number,
+}
+
+
+const dataMotorcycle: IMotorcycle[] = [
+  {
+    _id: "62905e472ab42302a46b5fdd",
+    model: "Honda CG Titan 125",
+    year: 1963,
+    color: "red",
+    buyValue: 3500,
+    category: "Custom",
+    engineCapacity: 2
+  }
+]
+
 export const MongooseMock = {
-  create: async (obj: ICar): Promise<ICar> => {
-    return {
-      ...obj,
-      _id: "62905e472ab42302a46b5fdf"
+  create: async (obj: ICar | IMotorcycle): Promise<ICar | IMotorcycle> => {
+    if ("category" in obj) {
+      return dataMotorcycle[0]
     }
+    return dataCar[0]
   },
 
   find: async (): Promise<ICar[]> => {
-    return data;
+    return dataCar;
   },
 
-  findOne: async({_id}: { _id: string }): Promise<ICar | null> => {
-    if (data[0]._id !== _id) return null;
-    return data[0];
+  findOne: async({_id}: { _id: string }): Promise<ICar | IMotorcycle | null> => {
+    if (dataCar[0]._id !== _id && dataMotorcycle[0]._id !== _id) return null;
+
+    if(_id === dataMotorcycle[0]._id) {
+      return dataMotorcycle[0]
+    }
+
+    return dataCar[0];
   },
 
-  findOneAndUpdate: async ({ _id }: {_id: string}, obj: ICar) => {
-    const findCar = data.find((car) => car._id === _id);
+  findOneAndUpdate: async ({ _id }: {_id: string}, obj: ICar)
+  : Promise<ICar | IMotorcycle | null> => {
+    const findCar = dataCar.find((car) => car._id === _id);
+    const findMotorcycle = dataMotorcycle.find((car) => car._id === _id);
 
-    if (!findCar) return null;
+    if (!findCar && !findMotorcycle) return null;
 
-    const newData = data.map(car => {
-      if (car._id === _id) return ({
-        ...car,
+    const newDataMotorcycle: IMotorcycle[] = dataMotorcycle.map(motorcycle => {
+      if (motorcycle._id === _id) return ({
+        ...motorcycle,
         ...obj
       })
-
-      return data[0];
-    })
+      return dataMotorcycle[0];
+    });
     
-    return newData[0];
+    const newDataCar: ICar[] = dataCar.map(car => {
+      if (car._id === _id) return (
+        {
+          ...car,
+          ...obj
+        })
+
+      return dataCar[0];
+    });
+    
+    return _id === dataMotorcycle[0]._id ? newDataMotorcycle[0] : newDataCar[0];
   },
 
   findOneAndDelete: async ({_id}: { _id: string }): Promise<ICar | null> => {
-    const findCar = data.find((car) => car._id === _id);
+    const findCar = dataCar.find((car) => car._id === _id);
+    const findMotorcycle = dataMotorcycle.find((car) => car._id === _id);
 
-    if (!findCar) return null;
+    if (!findCar && !findMotorcycle) return null;
 
-    return data[0];
+    return _id === dataMotorcycle[0]._id ? dataMotorcycle[0] : dataCar[0];
   }
 }
